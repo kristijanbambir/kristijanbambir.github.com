f533e2ed9f02838988e75fbbe7c076aa3d639af5
Kristijan <murdoc9889@gmail.com>
2011-02-25 20:16:53 +0100

    Redesign 001 update

--- redesign/001/js/script.js
+++ redesign/001/js/script.js
@@ -1,16 +1,16 @@
 ﻿$(document).ready(function() {
     
-    function adjustVerticalAlign() {
+    function adjustVerticalAlign(fix) {
         var documentHeight = $(document).height();
         var bodyHeight = $('body').height();
-        var marginTop = Math.round((documentHeight - bodyHeight) / 2) - 10;
-        $('body').css('padding-top', marginTop);
+        var marginTop = Math.round((documentHeight - bodyHeight) / 2) - 10 - fix;
+        $('body').css('margin-top', marginTop);
     }
     
-    adjustVerticalAlign();
+    adjustVerticalAlign(60);
     
     $(window).resize(function() {
-        adjustVerticalAlign();
+        adjustVerticalAlign(0);
     });
     
     $('div#content').hide();
@@ -21,7 +21,7 @@
     
     $('a').hover(
         function() {
-            $(this).stop().fadeTo('fast', 0.5);
+            $(this).stop().fadeTo(200, 0.5);
         },
         function() {
             $(this).stop().fadeTo(200, 1);
