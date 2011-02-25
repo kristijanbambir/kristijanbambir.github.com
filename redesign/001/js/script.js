603e5247aac04165de09d4e2bdfbed882e2f9625
Kristijan <murdoc9889@gmail.com>
2011-02-25 19:42:09 +0100

    Redesign 001 update

---
+++ redesign/001/js/script.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function() {
+    
+    function adjustVerticalAlign() {
+        var documentHeight = $(document).height();
+        var bodyHeight = $('body').height();
+        var marginTop = Math.round((documentHeight - bodyHeight) / 2) - 10;
+        $('body').css('padding-top', marginTop);
+    }
+    
+    adjustVerticalAlign();
+    
+    $(window).resize(function() {
+        adjustVerticalAlign();
+    });
+    
+    $('div#content').hide();
+    
+    $(window).mousemove(function() {
+       $('div#content').delay(500).fadeIn('slow'); 
+    });
+    
+    $('a').hover(
+        function() {
+            $(this).stop().fadeTo('fast', 0.5);
+        },
+        function() {
+            $(this).stop().fadeTo(200, 1);
+        }
+    );
+    
+});
