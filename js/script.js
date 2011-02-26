4302dfd77b2c0feac8d6dcfd5a955a341236bbda
Kristijan <murdoc9889@gmail.com>
2011-02-26 23:17:26 +0100

    Redesign

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 ﻿$(document).ready(function() {
     
-    function adjustVerticalAlign(fix) {
+    function adjustVerticalAlign() {
         var documentHeight = $(document).height();
         var bodyHeight = $('body').height();
-        var marginTop = Math.round((documentHeight - bodyHeight) / 2) - 10 - fix;
+        var marginTop = Math.round((documentHeight - bodyHeight) / 2) - 90;
         $('body').css('margin-top', marginTop);
     }
     
-    adjustVerticalAlign(0);
+    adjustVerticalAlign();
     
     $(window).resize(function() {
-        adjustVerticalAlign(0);
+        adjustVerticalAlign();
     });
     
     $('a').hover(
