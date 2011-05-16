a12bf47f1ecc4403b9001c6d4a1636af2bdcb47b
Kristijan <murdoc9889@gmail.com>
2011-05-16 23:33:48 +0200

    Added CSS3 presentation

---
+++ projects/002/src/jquery/script.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function() {
+    
+    var footertext = "FER diplomski seminar: Tehnologije Interneta i World Wide Weba - CSS3, Kristijan Bambir";
+    $('div.footer').html(footertext);
+    
+});
