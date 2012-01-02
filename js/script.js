9c0f60db8f55ab08a1c86b1e2692d8f0b6534624
KristijanBambir <kristijan.bambir@gmail.com>
2012-01-02 22:37:43 +0100

    New design

--- js/script.js
+++ js/script.js
@@ -1,25 +1,18 @@
-﻿$(document).ready(function() {
+﻿$(function () {
     
-    function adjustVerticalAlign() {
-        var documentHeight = $(document).height();
-        var bodyHeight = $('body').height();
-        var marginTop = Math.round((documentHeight - bodyHeight) / 2) - 90;
-        $('body').css('margin-top', marginTop);
-    }
+    var socialicon = $(".socialicon");
     
-    adjustVerticalAlign();
+    socialicon.fadeTo(0, 0.7);
     
-    $(window).resize(function() {
-        adjustVerticalAlign();
-    });
-    
-    $('a').hover(
-        function() {
-            $(this).stop().fadeTo(200, 0.5);
-        },
-        function() {
+    socialicon.hover(
+        function () {
             $(this).stop().fadeTo(200, 1);
+        }, 
+        function () {
+            $(this).stop().fadeTo(200, 0.7);
         }
     );
     
+    socialicon.tipsy({opacity: 0.6});
+
 });
