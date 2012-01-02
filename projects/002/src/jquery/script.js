c7f3e158572ffff4159fd491b669cffab8916b6c
KristijanBambir <kristijan.bambir@gmail.com>
2012-01-02 22:46:16 +0100

    CPAN removed

--- projects/002/src/jquery/script.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function() {
-    
-    var footertext = "FER diplomski seminar: Tehnologije Interneta i World Wide Weba - CSS3, Kristijan Bambir";
-    $('div.footer').html(footertext);
-    
-    $('span#left-init-key').hide();
-    
-});
