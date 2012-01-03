241f7690143ee3c88a8d881ccbdfa2c26c8e2404
KristijanBambir <kristijan.bambir@gmail.com>
2012-01-03 22:49:09 +0100

    New design

--- js/script.js
+++ js/script.js
@@ -13,6 +13,19 @@
         }
     );
     
-    socialicon.tipsy({opacity: 0.6});
+    socialicon.tipsy({opacity: 0.6, fade: true});
+    
+    /*function adjustVerticalAlign() {
+        var documentHeight = $(document).height();
+        var bodyHeight = $('body').height();
+        var marginTop = Math.round((documentHeight - bodyHeight) / 2);
+        $('body').css('margin-top', marginTop);
+    }
+    
+    adjustVerticalAlign();
+    
+    $(window).resize(function() {
+        adjustVerticalAlign();
+    });*/
 
 });
