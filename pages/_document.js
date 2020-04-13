17a11179e7fb1b24047d93e677882a156c180c00
Kristijan Bambir <kristijan.bambir@infinum.hr>
2020-04-13 19:18:08 +0200

    feat: add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,23 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Noto+Serif"
             rel="stylesheet"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
           <style>{`
             html {
               height: 100%;
