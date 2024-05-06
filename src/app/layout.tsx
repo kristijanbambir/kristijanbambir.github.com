05115c58f103e81b0aa92472441433c0d17edee2
Kristijan Bambir <kristijan.bambir@gmail.com>
2024-05-06 22:04:13 +0200

    Change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Playfair_Display as Font } from 'next/font/google';
 import './reset.css';
 import './globals.css';
 
-const font = Inter({ subsets: ['latin'] });
+const font = Font({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Kristijan Bambir',
